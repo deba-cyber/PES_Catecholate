bc71821f2ac277cb3e2c4c3146d1297b3e851b85
deba-cyber <64672586+deba-cyber@users.noreply.github.com>
2022-01-05 11:29:54 +0530

    Add files via upload

--- src/SD.cpp
+++ src/SD.cpp
@@ -4,11 +4,86 @@
 # include <vector>
 
 
+STRIDE_ARR_BACK_N_FORTH::STRIDE_ARR_BACK_N_FORTH(const std::vector<int>& basis_size_vect)	{
+	this->basis_size_vect = basis_size_vect;
+}
+
+std::vector<int> STRIDE_ARR_BACK_N_FORTH::stride_arr()	{
+	/** preparing stride array **/
+	int cur_product;
+	int cur_index_pdt = 1;
+	for (size_t j=1; j<basis_size_vect.size(); j++)	{
+		cur_index_pdt*= basis_size_vect[j];
+	}
+	/** Initialising stride array with first element **/
+	std::vector<int>stride_arr_init;
+	stride_arr_init.emplace_back(cur_index_pdt);
+	/** other elements of stride array will be prepared from the first element of the stride array **/
+	int cur_index_init = 1;		// initialising current index for generating other elements of stride array 
+	while (true)	{
+		if (cur_index_init == basis_size_vect.size()-1)	{
+			break;
+	}
+		else 
+		{
+			cur_product = int(cur_index_pdt/basis_size_vect[cur_index_init]);
+			cur_index_init +=1;
+			stride_arr_init.emplace_back(cur_product);
+			cur_index_pdt = cur_product;
+		}
+	}
+	return stride_arr_init;
+}
 
 
+/**********************************************************
+ ********* member function to generate multidimensional 
+			index from onedimensional index array  *******
+ **********************************************************/
+
+int STRIDE_ARR_BACK_N_FORTH::multidim_index_dpb(const std::vector<int> &onedim_index_vect)	{
+	/** Given one dimensional indices , will return multidimensional index 
+	 ** array/vector containing one dimensional indices are zero-based indices **/
+	std::vector<int>stride_arr = this->stride_arr();	// calling stride array
+	int multidim_basis_index = onedim_index_vect.back();
+	multidim_basis_index++;		// adding one for 1-based indexing 
+	for (size_t i=0; i<stride_arr.size(); i++)	{
+		multidim_basis_index += stride_arr[i]*onedim_index_vect[i];
+	}
+	return multidim_basis_index;
+}
+
+
+/********************************************************************************************
+ * ********* member function to one dimensional index array (returned as vector)	*********
+ *	*******		from multidimensional index (1-based indexing)
+ *******************************************************************************************/
+std::vector<int> STRIDE_ARR_BACK_N_FORTH::onedim_indices(const int & multidim_index)	{
+	/** Given multidimensional index for direct product basis, returns
+	 ** one dimensional indices ( 0- based indexing ); multidim_index has 1-based indexing ***/
+	std::vector<int>stride_arr = this->stride_arr();	// calling stride array 
+	int multidim_index_4_caln;
+	multidim_index_4_caln = multidim_index -1;
+	std::vector<int>onedim_index_vect;
+    /** multidim_index will change for finding each of the 1-d indices in the loop .. 
+	 * here it is first initialized **/
+	int cur_onedim_index;
+	for (size_t i=0; i<stride_arr.size(); i++)	{
+		cur_onedim_index = int(multidim_index_4_caln/stride_arr[i]);
+		onedim_index_vect.emplace_back(cur_onedim_index);
+		multidim_index_4_caln -= cur_onedim_index*stride_arr[i];
+	}
+	onedim_index_vect.emplace_back(multidim_index_4_caln);
+	/** returns 1 dimensional index array .. zero based indexing **/
+	return onedim_index_vect;
+}
+
+
+/***----------------------------------------------------------***/
+/***----------------------------------------------------------***/
 
 /** function for generating displacement 
- * grids for all coordinates for gradient calculation 
+ * grids for all coordinates of interest for gradient calculation 
  *  using 5-Pt numerical differentiation **/
 
 std::vector<std::vector<double>> get_all_onedim_grids_4_grad(const std::vector<double>& curgrid)	{
@@ -23,6 +98,9 @@ std::vector<std::vector<double>> get_all_onedim_grids_4_grad(const std::vector<d
 	}
 	return multgrid_4_numer_drv;
 }
+
+
+
 
 /** function to generate multidimensional grids 
  * for function evaluation at displaced grids 
@@ -42,5 +120,4 @@ std::vector<std::vector<double>> get_multidim_grid_from_all_onedim_grids(const s
 	}
 	return multigrid_4_grad;
 }
-
 
--- src/constants.cpp
+++ src/constants.cpp
@@ -19,15 +19,22 @@ namespace PES_PARAMS	{
 	extern const std::string diabat_cub_coeff_file = "../input_data/chk_morse_com_ref_cubic_9_pt";
 	extern const std::string diabat_quartic_coeff_file = "../input_data/chk_morse_com_ref_quartic_9_pt";
 	extern const std::string diabat_high_crr_file = "../input_data/QS_CRR_OFF_DIAG";
-	extern const std::string gridfile = "../input_data/contracted_setB_trial_3";
-	extern const std::string pot_savefile = "../output_data/Potential_setB_trial_3";
+	extern const std::string gridfile = "../input_data/E_IM_grid_RMS";
+	extern const std::string pot_savefile = "../output_data/Potential_IM_grid";
 	extern const std::vector<double> alpha_vect = {6.9, 2.58, 2.60, 2.50, 2.0, 2.0};
+	extern const std::string datasavetype = "dat";
 }
 
-namespace SD_PARAMS {
-    extern const std::vector<int> mode_id_vect = {1,5,7,10,13,27,29};
-    extern const std::vector<int> mode_id_2_optimise = {1,5,7,10,13,27,29};
-    extern const std::vector<double> startvect = {-2.0,0.5,0.1,2.8,-0.1,0.1,1.0};
+namespace SD_PARAMS	{
+	extern const std::string gridfile_2_optimise = "../input_data/Q1_Q10_relax_input_grid";
+	extern const std::string gridfile_2_start = "../input_data/Q1_Q10_relax_starvect";
+	extern const std::vector<int> mode_id_vect = {1,5,7,8,10,13,16,17,18,19,21,22,23,24,25,26,27,28,29,30,31,32,33};
+	extern const std::vector<int> mode_id_2_optimise = {1,5,7,8,13,16,17,18,19,21,22,23,24,25,26,27,28,30,31,32,33};
+//	extern const std::vector<int> mode_id_fixed = {1,5,10,29};
+//	extern const std::vector<double> startvect = {-2.0,0.5,0.3,2.8,-0.3,-0.116141,0.202347,0.152987,0.0969398,-0.225726,0.1,1.0};
+	extern const std::vector<double> startvect = {-2.86,0.55,0.34,0.07,2.77,-0.34,0.12,-0.09,-0.09,0.0075,-0.1942,-0.1485,0.1013,-0.2284,0.0088,-0.0952,0.2884,-0.0361,0.83,0.0034,-0.0003,0.0052,0.001};
+//	extern const std::vector<double> startvect = {7.180125e-01,5.605201e-01,-1.877045e-01,-5.937518e-01,
+//		-1.646329e-01,-7.822349e-02,9.285675e-02,-4.417326e-02,3.478453e-01,1.164222e-01,2.681571e-01,-3.813042e-01,
+//		5.104468e-02,-1.897087e-01,4.958839e-01,-6.497944e-02,1.457136e+00,-2.610049e-03,-8.922957e-03,9.870465e-04,
+//		-4.750531e-03};	
 }
-
-
